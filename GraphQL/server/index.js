818fd0955da4dae85300cf30099de28d0a82bc93
Kin Chan <xkinchanx@gmail.com>
2018-07-19 22:20:06 -0700

    [feat] server setup

---
+++ GraphQL/server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+import parser from 'body-parser';
+import router from './router';
+
+const app = express();
+const port = 3000;
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use('/api', router);
+
+app.listen(port, () => console.log('Server is listening on port: ', port));
