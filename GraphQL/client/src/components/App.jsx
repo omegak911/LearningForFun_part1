f13817cf3c68a9271f17d8c0df9cb68902056501
Kin Chan <xkinchanx@gmail.com>
2018-07-23 15:12:47 -0700

    [feat] will use state changes to switch between components

--- GraphQL/client/src/components/App.jsx
+++ GraphQL/client/src/components/App.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () =>
-  <div>
-    Hello from App
-  </div>
+import Login from '../components/Landing/Login';
+import Signup from '../components/Landing/Signup';
+import Home from '../components/Home/Home';
+import Nav from '../components/Nav/Nav';
+import Wild from '../components/Wild/Wild';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: true,
+      signup: false,
+      home: false,
+      wild: false,
+      currentPage: 'login',
+    }
+  }
+
+  handleLogin = (username, password) => {
+
+  }
+
+  handleSignup = (username, password) => {
+
+  }
+
+  handleNav = (navigateTo) => {
+    let { currentPage } = this.state;
+    this.setState({ [navigateTo]: true, [currentPage]: false, currentPage: navigateTo });
+  }
+
+  render() {
+    let { login, signup, home, wild, currentPage } = this.state;
+    return (
+      <div>
+        {login && <Login />}
+        {signup && <Signup />}
+        {!login && !signup && <Nav handleNav={this.handleNav}/>}
+        {home && <Home />}
+        {wild && <Wild />}
+      </div>
+    )
+  }
+}
 
 export default App;
