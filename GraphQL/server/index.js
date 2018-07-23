6cce59b19a4406b7573437e26407ca57e1e14e1f
Kin Chan <xkinchanx@gmail.com>
2018-07-23 16:31:12 -0700

    [refactor] one route

--- GraphQL/server/index.js
+++ GraphQL/server/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import path from 'path';
 import parser from 'body-parser';
-import router from './router';
-
+import graphqlHTTP from 'express-graphql'
 import db from '../database/index';
+import schema from './components/Schema';
 
 const app = express();
 const port = 3000;
@@ -12,6 +12,9 @@ app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.use('/api', router);
+app.use('/api', new graphqlHTTP({
+    schema,
+    graphiql: true
+  }))
 
 app.listen(port, () => console.log('Server is listening on port: ', port));
