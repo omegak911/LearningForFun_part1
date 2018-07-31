40601cc41b431ccbf7a3cdeaf55301658569b02c
Kin Chan <xkinchanx@gmail.com>
2018-07-30 22:06:21 -0700

    [prog] caught counter/buttons and css

--- Vue/exercises/Practice/src/components/PokemonListItem.vue
+++ Vue/exercises/Practice/src/components/PokemonListItem.vue
@@ -4,19 +4,31 @@
     <p>ID: {{ id }}</p>
     <p>Name: {{ name }}</p>
     <p>Type: {{ type }}</p>
+    <div class="caughtButtons">
+      <button @click="caught === 0 ? 0 : caught -= 1">-</button>
+      <p>Caught: {{ caught }}</p>
+      <button @click="caught += 1">+</button>
+    </div>
   </div>
 </template>
-
 
 <script>
   export default {
     data() {
       return {
-
+        caught: 0
       }
     },
     props: ["img", "id", "name", "type"],
   }
-
 </script>
 
+<style scoped>
+  .caughtButtons {
+    display: flex;
+    justify-content: space-between;
+    margin-left: 10%;
+    margin-right: 10%;
+    margin-bottom: 1%;
+  }
+</style>
